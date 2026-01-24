4d5c0e31ca37d6027936d01e1252e55e48398456
Alan Webb <webbalan91939@gmail.com>
2026-01-24 19:30:19 +0000

    Create Services component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Services from "./components/Services";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
 			<Hero />
 			<Banner />
 			<Pricing />
+			<Services />
 			<Footer />
 		</div>
 	);
