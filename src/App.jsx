af2e702870f3361ca2a36096b738393ab82ddb3f
Alan Webb <webbalan91939@gmail.com>
2026-01-31 11:35:59 +0000

    Create various components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Banner from "./components/Banner";
+import TopBar from "./components/TopBar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Pricing from "./components/Pricing";
-import Services from "./components/Services";
+import Activities from "./components/Activities";
 import Footer from "./components/Footer";
 
 const App = () => {
 	return (
 		<>
+			<TopBar />
+			<Navbar />
 			<Hero />
-			<Banner />
-			<Pricing />
-			<Services />
+			<Activities />
 			<Footer />
 		</>
 	);
