a308e7c64096d9ea36f9c3def9d322663d0d916f
Alan Webb <webbalan91939@gmail.com>
2026-02-07 12:38:33 +0000

    Update TopBar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,5 +1,15 @@
+import {FaPhone, FaClock} from "react-icons/fa";
+import {BsChatSquareDotsFill} from "react-icons/bs";
+
 const TopBar = () => {
-	return <div>TopBar</div>;
+	return (
+		<div className="bg-teal-600">
+			<div>
+				<BsChatSquareDotsFill />
+				<h1 className="uppercase">Minimal Living</h1>
+			</div>
+		</div>
+	);
 };
 
 export default TopBar;
