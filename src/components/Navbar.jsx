669a78f840121d1f738d92686bf74ee597c03802
Alan Webb <webbalan91939@gmail.com>
2026-02-09 16:46:50 +0000

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
+import {
+	FaBars,
+	FaTimes,
+	FaFacebook,
+	FaInstagramSquare,
+	FaYoutube,
+} from "react-icons/fa";
+import {FaSquareXTwitter} from "react-icons/fa6";
+import {BsFillChatSquareDotsFill} from "react-icons/bs";
+
 const Navbar = () => {
 	return <div>Navbar</div>;
 };
