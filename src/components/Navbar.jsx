5c98401b18f2f9ba140ccd8299953344eef60f47
Alan Webb <webbalan91939@gmail.com>
2026-02-10 08:45:16 +0000

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from "react";
 import {
 	FaBars,
 	FaTimes,
@@ -9,7 +10,30 @@ import {FaSquareXTwitter} from "react-icons/fa6";
 import {BsFillChatSquareDotsFill} from "react-icons/bs";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div>
+			<ul>
+				<li>
+					<a href="/">Home</a>
+				</li>
+				<li>
+					<a href="#gallery">Gallery</a>
+				</li>
+				<li>
+					<a href="#deals">Deals</a>
+				</li>
+				<li>
+					<a href="#contact">Contact</a>
+				</li>
+			</ul>
+			<div>
+				<FaFacebook className="mx-4" />
+				<FaSquareXTwitter className="mx-4" />
+				<FaInstagramSquare className="mx-4" />
+				<FaYoutube className="mx-4" />
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
