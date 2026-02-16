bb1bd02e6b98a632be3da79f7a63489b9b9d5eab
Alan Webb <webbalan91939@gmail.com>
2026-02-16 07:49:18 +0000

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ import {BsFillChatSquareDotsFill} from "react-icons/bs";
 const Navbar = () => {
 	return (
 		<div className="w-full min-h-12.5 flex justify-between items-center absolute z-10 text-white bg-green-800">
-			<ul>
+			<ul className="hidden sm:flex px-4">
 				<li>
 					<a href="/">Home</a>
 				</li>
@@ -26,18 +26,19 @@ const Navbar = () => {
 					<a href="#contact">Contact</a>
 				</li>
 			</ul>
-			<div>
+			<div className="flex justify-between">
 				<FaFacebook className="mx-4" />
 				<FaSquareXTwitter className="mx-4" />
 				<FaInstagramSquare className="mx-4" />
 				<FaYoutube className="mx-4" />
 			</div>
 			{/* Hamburger Icon */}
-			<div>
-				<FaBars />
+			<div className="sm:hidden z-10">
+				<FaBars size={20} className="mr-4 cursor-pointer" />
 			</div>
 			{/* Mobile Menu */}
-			<div>
+			<div
+				className={`overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-teal-900/90 px-4 py-7 flex flex-col`}>
 				<ul>
 					<li>
 						<a href="/">Home</a>
