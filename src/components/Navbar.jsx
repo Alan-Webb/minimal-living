6a74eb6334ab66abb1eccf1a64502d4dbafef1e0
Alan Webb <webbalan91939@gmail.com>
2026-02-17 07:09:14 +0000

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,15 @@ import {FaSquareXTwitter} from "react-icons/fa6";
 import {BsFillChatSquareDotsFill} from "react-icons/bs";
 
 const Navbar = () => {
+	const [mobileMenu, setMobileMenu] = useState(false);
+
+	const handleMobileMenu = () => {
+		setMobileMenu(!mobileMenu);
+	};
+
 	return (
 		<div className="w-full min-h-12.5 flex justify-between items-center absolute z-10 text-white bg-green-800">
-			<ul className="hidden sm:flex px-4">
+			<ul className="hidden sm:flex px-4 space-x-6 uppercase tracking-wider">
 				<li>
 					<a href="/">Home</a>
 				</li>
@@ -33,13 +39,17 @@ const Navbar = () => {
 				<FaYoutube className="mx-4" />
 			</div>
 			{/* Hamburger Icon */}
-			<div className="sm:hidden z-10">
+			<div onClick={handleMobileMenu} className="sm:hidden z-10">
 				<FaBars size={20} className="mr-4 cursor-pointer" />
 			</div>
 			{/* Mobile Menu */}
 			<div
-				className={`overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-teal-900/90 px-4 py-7 flex flex-col`}>
-				<ul>
+				className={
+					mobileMenu
+						? "overflow-y-hidden md:hidden ease-in duration-300 absolute text-white text-3xl left-0 top-0 w-full h-screen bg-teal-600/90 px-4 py-7 flex flex-col"
+						: "absolute top-0 h-screen left-full ease-in duration-500"
+				}>
+				<ul className="h-full w-full text-center pt-12 space-y-6 uppercase tracking-wider">
 					<li>
 						<a href="/">Home</a>
 					</li>
