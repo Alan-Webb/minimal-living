91d19c8b6faea30f2bdf6dc6b205365f2ffbd4b2
Alan Webb <webbalan91939@gmail.com>
2026-02-20 01:25:55 +0000

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,12 @@ const Hero = () => {
 	return (
 		<div>
 			<img src="./src/assets/heroBg.webp" alt="cabin in woods" />
-			{/* <h2>Remote cabins for focused minds.</h2>
+			<div>
+				<div>
+					<h1>Remote cabins for focused minds.</h1>
+				</div>
+			</div>
+			{/* 
 			<p>
 				Minimal Living offers thoughtfully chosen remote cabins for corporate
 				retreats that value focus over excess. Step away from notifications and
