88c8418eb61ed7cc10e7ffbd46cabbb1cbbb8d06
Alan Webb <webbalan91939@gmail.com>
2026-02-21 12:19:38 +0000

    Update Hero & CSS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,15 +5,14 @@ const Hero = () => {
 			<div>
 				<div>
 					<h1>Remote cabins for focused minds.</h1>
+					<p>
+						Minimal Living offers thoughtfully chosen remote cabins for
+						corporate retreats that value focus over excess. Step away from
+						notifications and meeting rooms, and into an environment built for
+						alignment, reflection, and meaningful progress.
+					</p>
 				</div>
 			</div>
-			{/* 
-			<p>
-				Minimal Living offers thoughtfully chosen remote cabins for corporate
-				retreats that value focus over excess. Step away from notifications and
-				meeting rooms, and into an environment built for alignment, reflection,
-				and meaningful progress.
-			</p> */}
 		</div>
 	);
 };
