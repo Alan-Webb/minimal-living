281d66dc334263adbe8a037688c52097894c0ec2
Alan Webb <webbalan91939@gmail.com>
2026-02-25 12:27:06 +0000

    Update assets & Activities

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,10 @@ const Hero = () => {
 			/>
 			<div className="max-w-285 m-auto">
 				<div className="absolute top-[40%] w-full md:-[50%] max-w-150 h-full flex flex-col text-white tracking-wider p-4">
-					<h1 className="font-bold text-5xl pb-4">
-						Remote cabins for focused minds
+					<h1 className="font-bold text-4xl pb-4">
+						Remote getaways for focused minds
 					</h1>
-					<p className="text-4xl italic">
+					<p className="text-3xl italic">
 						Minimal Living offers thoughtfully chosen remote cabins for
 						corporate retreats that value focus over excess. Step away from
 						notifications and meeting rooms, and into an environment built for
